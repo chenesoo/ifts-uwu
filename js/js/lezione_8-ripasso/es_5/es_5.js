b48d19b347e8c60eeb64a272e7622d2bf86255d5
IDK <79741040+chenesoo@users.noreply.github.com>
2023-03-30 10:22:27 +0200

    ex

--- js/js/lezione_8-ripasso/es_5/es_5.js
+++
@@ -1,8 +0,0 @@
-function toggleTesto() {
-    let testo = document.getElementById("testo");
-    if (testo.className === "d-none") {
-        testo.className = "";
-    } else {
-        testo.className = "d-none";
-    }
-}
